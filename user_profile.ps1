1a6793bab2d233a140bb051e07da38f69379caf2
Francesco Xia <s276509@studenti.polito.it>
2022-01-10 16:46:53 +0100

    first commit

---
+++ user_profile.ps1
@@ -0,0 +1,29 @@
+# Aliases
+Set-Alias vim nvim
+Set-Alias ll ls
+
+# Imports
+Import-Module PSFzf
+Import-Module PSReadLine
+Import-Module Terminal-icons
+Import-Module oh-my-posh
+Import-Module z
+
+# run oh-my-posh
+$PROMPT_CONFIG = [IO.Path]::Combine($PSScriptRoot, "themes", "stelbent.minimal.omp.json")
+oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
+
+# PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+# Set-PSReadLineOption -EditMode Vi
+
+# Fzf
+Set-PsFzfOption -PSReadlineChordProvider "Ctrl+f" -PSReadlineChordReverseHistory "Ctrl+r"
+
+# Utilities
+function which ($exec) {
+    Get-Command -Name $exec -ErrorAction SilentlyContinue |
+        Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
